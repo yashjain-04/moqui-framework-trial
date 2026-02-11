f970e0ed4ed55453608aaec5836744b4d320dd5b
yashjain-04 <yashjain.yj04@gmail.com>
2026-02-11 14:27:31 +0530

    enforce party type validation and finalize REST person creation

--- party/service/party/createPerson.groovy
+++ party/service/party/createPerson.groovy
@@ -3,7 +3,7 @@ import org.moqui.entity.EntityValue
 
 ExecutionContext ec = context.ec
 
-// 1. Verify that a Party record exists
+// 1. Find the Party AND check its type
 EntityValue party = ec.entity.find("party.Party").condition("partyId", partyId).one()
 
 if (!party) {
@@ -11,9 +11,37 @@ if (!party) {
     return
 }
 
-// 2. Create the Person record using the implicit entity-auto service
+// NEW: Validate that the Party is actually a PERSON
+if (party.partyTypeEnumId != "PERSON") {
+    ec.message.addError("Cannot create Person. Party [${partyId}] is a ${party.partyTypeEnumId}, not a PERSON.")
+    return
+}
+
+// 2. Create the Person record
+// This will now only run if the Party exists AND is the correct type
 Map createResult = ec.service.sync().name("create", "party.Person").parameters(context).call()
 
-// 3. Return the exact response string requested
+// 3. Success Response
 message = "Person ${firstName} ${lastName} created successfully!"
 return [message: message]
+
+
+//import org.moqui.context.ExecutionContext
+//import org.moqui.entity.EntityValue
+//
+//ExecutionContext ec = context.ec
+//
+//// 1. Verify that a Party record exists
+//EntityValue party = ec.entity.find("party.Party").condition("partyId", partyId).one()
+//
+//if (!party) {
+//    ec.message.addError("Cannot create Person. Party with ID [${partyId}] does not exist.")
+//    return
+//}
+//
+//// 2. Create the Person record using the implicit entity-auto service
+//Map createResult = ec.service.sync().name("create", "party.Person").parameters(context).call()
+//
+//// 3. Return the exact response string requested
+//message = "Person ${firstName} ${lastName} created successfully!"
+//return [message: message]
--- tutorial/screen/tutorial/createTutorial.groovy
+++ tutorial/screen/tutorial/createTutorial.groovy
@@ -1,4 +1,4 @@
-def tutorial = ec.entity.makeValue("tutorial.Tutorial")
-tutorial.setFields(context, true, null, null)
-if (!tutorial.tutorialId) tutorial.setSequencedIdPrimary()
-tutorial.create()
+def tutorial = ec.entity.makeValue("tutorial.Tutorial") // Initialize the object.	Empty in RAM.
+tutorial.setFields(context, true, null, null) // Fill the object with data.	Filled in RAM.
+if (!tutorial.tutorialId) tutorial.setSequencedIdPrimary() // Generate the Unique ID.	Ready to Save.
+tutorial.create() // Write to the Database.	Saved in MySQL.Write to the Database.	Saved in MySQL.
